4e365f2acd68db8abcab44efa04215b1742ceb8e
caleb <ck.caleb17@gmail.com>
2019-02-11 21:13:50 +0300

    add divide function

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+var add = function(number1, number2) {
+return number1 + number2;
+};
+alert(add(3, 5));
+var subtract = function(number1, number2) {
+return number1 - number2;
+};
+alert(subtract(10, 5));
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var number1 = prompt("Enter a number:");
+alert(number1);
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(add(number1, number2));
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(add(number1, number2));
+var subtract= function(number1, number2) {
+  return number1 + number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = subtract(number1, number2);
+alert(result);
+var multiply = function(number1, number2) {
+  return number1 + number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = multiply(number1, number2);
+alert(result);
+var divide = function(number1, number2) {
+  return number1 + number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = divide(number1, number2);
+alert(result);
