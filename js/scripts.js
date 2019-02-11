bd9ce67ccadfef358fe8fdd1cdf0b6d5c379ead3
caleb <ck.caleb17@gmail.com>
2019-02-11 21:16:01 +0300

    add initial page structure

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,7 @@ var subtract= function(number1, number2) {
 };
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = subtract(number1, number2);
+var result = subtract (number1, number2);
 alert(result);
 var multiply = function(number1, number2) {
   return number1 + number2;
