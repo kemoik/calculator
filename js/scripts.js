befdff8a1626346eea5afff181c8ba4b032f5802
caleb <ck.caleb17@gmail.com>
2019-02-11 21:14:27 +0300

    add multiply function

--- js/scripts.js
+++ js/scripts.js
@@ -35,7 +35,7 @@ var multiply = function(number1, number2) {
 };
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = multiply(number1, number2);
+var result = multiply (number1, number2);
 alert(result);
 var divide = function(number1, number2) {
   return number1 + number2;
